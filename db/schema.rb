2de33a59788c6dac2376d4b2909b86818d47ac62
Quinn Krug <quinn.krug@gmail.com>
2020-04-20 12:37:23 -0600

    create chef model file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_20_182723) do
+ActiveRecord::Schema.define(version: 2020_04_20_183146) do
+
+  create_table "chefs", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
