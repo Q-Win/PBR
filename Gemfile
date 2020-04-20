fd4094a8edd85e179e9a41e8747ffa3dbfd3412a
Quinn Krug <quinn.krug@gmail.com>
2020-04-20 16:26:43 -0600

    test to see recipes populate on index

--- Gemfile
+++ Gemfile
@@ -41,6 +41,7 @@ gem 'bootsnap', '>= 1.1.0', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'sqlite3'
+  gem 'rails-controller-testing'
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
 end
 
--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -1,10 +1,23 @@
 require 'test_helper'
 
 class RecipesTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = Chef.create(name: "Bob", email: "Bob@Robert.com")
+    @recipe = Recipe.create(name: "vegetable saute", description: "great vegetable sautee", chef: @user)
+    @recipe2 = @user.recipes.create(name: "chana masala", description: "chick peas")
+  end
+
   test "should get recipes index" do
     get recipes_path
     assert_response :success
   end
 
-  
+  test "Should get recipes listing" do
+    get recipes_path
+    assert_template 'recipes/index'
+    assert_match @recipe.name, response.body
+    assert_match @recipe2.name, repsonse.body
+  end
+
 end
