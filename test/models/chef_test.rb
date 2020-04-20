a15e2b61cbd30ffa9614d2db6b9112f84378e0b1
Quinn Krug <quinn.krug@gmail.com>
2020-04-20 14:11:43 -0600

    user model testing

---
+++ test/models/chef_test.rb
@@ -0,0 +1,63 @@
+require 'test_helper'
+
+class ChefTest < ActiveSupport::TestCase
+
+  def setup
+    @chef = Chef.new(name: "Bob", email: "bob@example.com")
+  end
+
+  test "should be valid" do
+    assert @chef.valid?
+  end
+
+  test "name should be present" do
+    @chef.name = ""
+    assert_not @chef.valid?
+  end
+
+  test "name should be less than 30 characters" do
+    @chef.name = "q" *31
+    assert_not @chef.valid?
+  end
+
+  test "email should be present" do
+    @chef.email = ""
+    assert_not @chef.valid?
+  end
+
+  test "email should not be too long" do
+    @chef.email = "t" *245 + "@example.com"
+    assert_not @chef.valid?
+  end
+
+  test "email should be valid format" do
+    valid_emails = %w[user@example.com bob@example.com steve@example.com]
+    valid_emails.each do |valids|
+      @chef.email = valids
+      assert @chef.valid?, "#{valids.inspect} should be valid"
+    end
+  end
+
+  test "should reject invalid email address" do
+    invalid_emails = %w[bob@example steve@yahoo,com Sett@gmail. Ashe@u+suck.com]
+    invalid_emails.each do |invalid_email|
+      @chef.email = invalid_email
+      assert_not @chef.valid?, "#{invalid_email.inspect} should be valid"
+    end
+  end
+
+  test "email should be unique and case insensitive" do
+    duplicate_chef = @chef.dup
+    duplicate_chef.email = @chef.email.upcase
+    @chef.save
+    assert_not duplicate_chef.valid?
+  end
+
+  test "email should be lowercase before hitting database" do
+    mixed_email = "joHn@example.com"
+    @chef.email = mixed_email
+    @chef.save
+    assert_equal mixed_email.downcase, @chef.reload.email
+  end
+
+end
--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -10,4 +10,24 @@ class RecipeTest < ActiveSupport::TestCase
     assert @recipe.valid?
   end
 
+  test "name should be present" do
+    @recipe.name = " "
+    assert_not @recipe.valid?
+  end
+
+  test "description should be present" do
+    @recipe.description = " "
+    assert_not @recipe.valid?
+  end
+
+  test "description should NOT be less than 5 characters" do
+    @recipe.description = "aaa"
+    assert_not @recipe.valid?
+  end
+
+  test "description should NOT be more than 500 characters" do 
+    @recipe.description = "a"*501
+    assert_not @recipe.valid?
+  end
+
 end
