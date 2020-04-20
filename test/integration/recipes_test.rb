6435a3ad366f69d465d2a0f79b7029b1d86d0d0a
Quinn Krug <quinn.krug@gmail.com>
2020-04-20 16:09:36 -0600

    create recipes controller

--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class RecipesTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get recipes index" do
+    get recipes_path
+    assert_response :success
+  end
+
+  
 end
