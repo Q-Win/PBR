b90c6176009eb0e80d179e0e3962f8c113f1e68e
Quinn Krug <quinn.krug@gmail.com>
2020-04-21 15:20:23 -0600

    set recipe to save if valid

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -13,7 +13,24 @@ class RecipesController < ApplicationController
   end
 
   def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.chef = Chef.first
+    if @recipe.save
+      flash[:success] = "Recipe was created successfully!"
+      redirect_to recipe_path(@recipe)
+    else
+      render 'new'
+    end
+  end
 
+  private
+
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :description, :image, ingredient_ids: [])
   end
 
 end
