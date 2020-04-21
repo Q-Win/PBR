a522a94e4f36a946d7ca1755f74ee17f102f98b8
Quinn Krug <quinn.krug@gmail.com>
2020-04-21 15:42:05 -0600

    set up test for successfully creating a recipe

--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -31,7 +31,14 @@ class RecipesTest < ActionDispatch::IntegrationTest
   test "create new valid recipe" do
     get new_recipe_path
     assert_template 'recipes/new'
-    
+    name_of_recipe = "veg saute"
+    description_of_recipe = " add vegetables, cook for 20 minutes, serve delicious meal"
+    assert_difference 'Recipe.count', 1 do
+      post recipes_path, params: { recipe: { name: name_of_recipe, description: description_of_recipe}}
+    end
+    follow_redirect!
+    assert_match name_of_recipe.capitalize, response.body
+    assert_match description_of_recipe, response.body
   end
 
   test "reject invalid submissions" do
