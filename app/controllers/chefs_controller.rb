de4e1be983aeaddf725b7e997ca065e081bec0c5
Quinn Krug <quinn.krug@gmail.com>
2020-04-22 13:44:03 -0600

    test for invlaid sign up passing

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -5,7 +5,18 @@ class ChefsController < ApplicationController
   end
 
   def create
+    @chef = Chef.new(chef_params)
+    if @chef.save
 
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def chef_params
+    params.require(:chef).permit(:name,:email,:passowrd,:password_confirmation)
   end
 
 end
