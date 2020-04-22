f0b87db69c74d7b221353626ae799d1a2a369bea
Quinn Krug <quinn.krug@gmail.com>
2020-04-22 13:36:45 -0600

    create chef sign up

--- test/integration/chefs_signup_test.rb
+++ test/integration/chefs_signup_test.rb
@@ -6,4 +6,12 @@ class ChefsSignupTest < ActionDispatch::IntegrationTest
     get signup_path
     assert_response :success
   end
+
+  test "reject an invalid signup" do
+
+  end
+
+  test "accept valid signup" do
+
+  end 
 end
