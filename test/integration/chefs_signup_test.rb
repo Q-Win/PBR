c429fe8a7648f9f0e5d1268788438121e224f861
Quinn Krug <quinn.krug@gmail.com>
2020-04-22 13:18:28 -0600

    add chef routes

--- test/integration/chefs_signup_test.rb
+++ test/integration/chefs_signup_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class ChefsSignupTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should get signup path" do
+    get signup_path
+    assert_response :success
+  end
 end
