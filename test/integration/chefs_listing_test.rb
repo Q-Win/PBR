dcaf3abd23567cc190bcde620b40c042138209a4
Quinn Krug <quinn.krug@gmail.com>
2020-04-23 10:46:38 -0600

    create test delete test

---
+++ test/integration/chefs_listing_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+class ChefsShowTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = Chef.create(name: "Bob", email: "Bob@Robert.com",
+                        password: "password", password_confirmation: "password")
+    @recipe = Recipe.create(name: "vegetable saute", description: "great vegetable sautee", chef: @user)
+    @recipe2 = @user.recipes.create(name: "chana masala", description: "chick peas")
+    @user2 = Chef.create(name: "Steve", email: "Steve@Robert.com",
+                        password: "password", password_confirmation: "password")
+  end
+
+  test "should delete chef" do
+    get chefs_path
+    assert_template 'chefs/index'
+    assert_difference 'Chef.count', -1 do
+      delete chef_path(@chef2)
+    end
+    assert_redirected_to chefs_path
+    assert_not flash.empty?
+  end
+
+
+
+end
