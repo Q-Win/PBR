cbf2b0ae967e37f4b39e778406569d63833b12b7
Quinn Krug <quinn.krug@gmail.com>
2020-04-27 13:07:29 -0600

    set up ingredient index view

--- app/controllers/ingredeints_controller.rb
+++ app/controllers/ingredeints_controller.rb
@@ -1,3 +1,27 @@
 class IngredientsController < ApplicationController
 
+  def show
+
+  end
+
+  def index
+    @ingredients = Ingredient.paginate(page: params[:page], per_page: 5)
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def updates
+
+  end
+
 end
