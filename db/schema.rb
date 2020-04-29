297403de254124a36c5dc5dd92fd5384d2f1aefe
Quinn Krug <quinn.krug@gmail.com>
2020-04-29 14:55:07 -0600

    create migration to add instructions to recipes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_29_193021) do
+ActiveRecord::Schema.define(version: 2020_04_29_205239) do
 
   create_table "chefs", force: :cascade do |t|
     t.string "name"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2020_04_29_193021) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "chef_id"
+    t.text "instructions"
   end
 
 end
