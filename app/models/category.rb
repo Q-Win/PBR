2d74d45a59fbb8f5d84d0b6b4ebfe3cbaee28624
Quinn Krug <quinn.krug@gmail.com>
2020-05-08 10:53:49 -0600

    category index test passing

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ApplicationRecord
+  before_save { self.name = name.downcase }
   validates :name, presence: true
   has_many :category_recipes
   has_many :recipes, through: :category_recipes
--- spec/features/categories/category_index_spec.rb
+++ spec/features/categories/category_index_spec.rb
@@ -4,15 +4,15 @@ RSpec.describe "categories_index", type: :feature do
 
   before(:each) do
     @category1 = Category.create(name: "Oil Free")
-    @category1 = Category.create(name: "Mexican")
-    @category1 = Category.create(name: "Italian")
+    @category2 = Category.create(name: "Mexican")
+    @category3 = Category.create(name: "Italian")
   end
 
   it "displays all categories on the index" do
     visit categories_path
 
-    expect(page).to have_content(@recipe1.name)
-    expect(page).to have_content(@recipe3.name)
-    expect(page).to have_content(@recipe2.name)
+    expect(page).to have_content(@category1.name)
+    expect(page).to have_content(@category3.name)
+    expect(page).to have_content(@category2.name)
   end
 end
