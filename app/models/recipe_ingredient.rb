b347a0c8850940fefa323f11eb28833029c1d8e0
Quinn Krug <quinn.krug@gmail.com>
2020-06-23 16:26:03 -0600

    set display name automatically on recipe_ingredient

--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -1,6 +1,11 @@
 class RecipeIngredient < ApplicationRecord
-  
+
   belongs_to :ingredient
   belongs_to :recipe
+
+  before_validation do |ri|
+    if ri.display_name == nil
+      ri.display_name = Ingredient.find(ri.ingredient_id).name
+  end
 
 end
