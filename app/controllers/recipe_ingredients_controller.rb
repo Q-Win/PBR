ab95ca9740fb360385762fafc32a3af52b62c5e1
Quinn Krug <quinn.krug@gmail.com>
2020-06-24 17:26:52 -0600

    add authorization for recipe_ingredient edit

--- app/controllers/recipe_ingredients_controller.rb
+++ app/controllers/recipe_ingredients_controller.rb
@@ -1,4 +1,6 @@
 class RecipeIngredientsController < ApplicationController
+  before_action :require_user, only: [:index]
+
   def index
     @recipe = Recipe.find(params[:recipe_id])
     @ingredients = @recipe.recipe_ingredients
@@ -7,7 +9,7 @@ class RecipeIngredientsController < ApplicationController
 
 
   def update
-    
+
     @recipe_ingredient = RecipeIngredient.find(params[:id])
     @recipe = Recipe.find(@recipe_ingredient.recipe_id)
     if @recipe_ingredient.update(recipe_ingredient_params)
@@ -22,6 +24,13 @@ class RecipeIngredientsController < ApplicationController
 
   def recipe_ingredient_params
     params.require(:recipe_ingredient).permit(:display_name, :unit, :quantity)
+  end
+
+  def require_same_user
+    if current_user != @recipe.chef and !current_user.admin?
+      flash[:danger] = "You can only edit or delete your own recipes"
+      redirect_to recipes_path
+    end
   end
 
 end
