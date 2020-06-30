3f476d73ce87978fd68ce3cf5e3f5610ccef6a55
Quinn Krug <quinn.krug@gmail.com>
2020-06-30 10:07:01 -0600

    comment out everything in getri

--- app/controllers/recipe_ingredients_controller.rb
+++ app/controllers/recipe_ingredients_controller.rb
@@ -26,7 +26,7 @@ class RecipeIngredientsController < ApplicationController
 
     recipe_ingredients.each do |ri|
       # recipe_id = Recipe.find_by(name: ri[:rname]).id
-      ingredient_id = Ingredient.find_by(name: ri[:iname]).id
+      # ingredient_id = Ingredient.find_by(name: ri[:iname]).id
       # if (ingredient_id != nil && recipe_id != nil)
       #   RecipeIngredient.create(recipe_id: recipe_id, ingredient_id: ingredient_id, unit: ri[:unit], quantity: ri[:quantity])
       # end
